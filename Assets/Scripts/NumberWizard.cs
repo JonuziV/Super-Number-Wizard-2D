839597807088fc4fe3bb3e859b90341d4a30864b
JonuziV <60705385+JonuziV@users.noreply.github.com>
2021-10-08 21:41:14 +0200

    Initial commit

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour
+{
+    // Start is called before the first frame update
+     [SerializeField] int max = 1000;
+     [SerializeField] int min = 1;
+     [SerializeField] Text guessText;
+     int guess = 500;
+    void Start()
+    {
+        StartGame();  
+    }
+
+    void StartGame()
+    {
+        NextGuess();
+    }
+    // Update is called once per frame
+    public void onPressHigher()
+    {
+        min = guess + 1;
+        NextGuess();
+    }
+
+    public void onPressLower()
+    {
+        max = guess;
+        NextGuess();
+    }
+    void NextGuess()
+    {
+      guess = Random.Range( min, max + 1);
+      guessText.text = guess.ToString();
+    }
+}
+
